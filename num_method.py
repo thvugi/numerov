76053f9c844f61b25fd1f4f4d5b1d1f7737c6271
thvugi <thvugi@gmail.com>
2024-04-10 10:41:04 -0400

    second push

--- num_method.py
+++ num_method.py
@@ -15,19 +15,15 @@ for i in range(n):
 
 
 def V(s):
-    #return abs(s)#((omega**2)*s**2)/2
     R=1.65
     return -60.9*math.exp(-(s/R)**2)
-    #if(s>1):
-   #     return 0
-   # if(s<=1):
-   #     return -100
-
+    
 LHS = 0
 RHS = 20
 L = RHS-LHS
 
 N = 1000
+M = 300
 
 delx = L/N
 
@@ -38,7 +34,7 @@ for j in range(N+1):
     VV.append(V(x))
 for j in range(N+1):
     x = LHS+delx*j
-    nmb = (1/(h2m))*(-10+V(x))
+    nmb = (1/(h2m))*(-2-V(x))
     U.append(nmb)
 
 BC = 0
@@ -46,24 +42,21 @@ BC = 0
 def BC1(x):
    return x
 
-psipos = []
-psineg=[]
-psipos.append(BC)
-psipos.append(BC1(LHS+delx))
-psineg.append(BC)
-psineg.append(BC1((math.exp(0.01*delx))))
+psipos = np.zeros(N+1)
+psineg=np.zeros(N+1)
+psipos[0]=0
+psipos[1]=delx
+psineg[N]=math.exp(-0.05*RHS)
+psineg[N-1]=math.exp(-0.05*(RHS+delx))
 
-for i in range(1,int(N/2)-1):
-        temp = (psipos[i-1]*(12-(delx**2)*U[i-1])-2*psipos[i]*((5*delx**2)*U[i]+12))/((delx**2)*U[i+1]-12)
-        psipos.append(temp)
-    
-num = N
+for i in range(1,M+1):
+        psipos[i+1] = (-psipos[i-1]*(12+(delx**2)*U[i-1])+2*psipos[i]*((-5*delx**2)*U[i]+12))/((delx**2)*U[i+1]+12)
 
 
-for j in range(1,int(N/2)-1):
-        temp = (psineg[j-1]*((delx**2)*U[num]-12)+2*psineg[j]*((5*delx**2)*U[num-1]+12))/(12-(delx**2)*U[num-2])
-        num=num-1
-        psineg.append(temp)
+for j in range(N-1,M-1,-1):
+         psineg[j-1] = (-psineg[j+1]*((delx**2)*U[j+1]+12)+2*psineg[j]*(12-(5*delx**2)*U[j]))/(12+(delx**2)*U[j-1])
+        
+        
 
 #plt.plot(VV)
 plt.plot(psipos,linewidth=1.5,color='red')
